851e5e0dafa1fe713ff7a66412cf7909ed091c25
Milton <milton.cronvall@elev.ga.ntig.se>
2022-10-17 14:18:13 +0200

    asd

--- form/config/config.php
+++ form/config/config.php
@@ -1,8 +1,8 @@
 <?php
-/* 
-const DB_HOSTNAME = "";
-const DB_USERNAME = "";
-const DB_PASSWORD = "";
-const DB_NAME     = "";
-*/
+
+$DB_HOSTNAME = "localhost";
+$DB_USERNAME = "root";
+$DB_PASSWORD = "";
+$DB_NAME     = "";
+
 ?>
