29246da696427a3d93c3c322155f6b36ca2863d9
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-11-09 00:28:49 +0100

    New stuff added; code improvements

--- form/content/newpost_req.php
+++ form/content/newpost_req.php
@@ -1,7 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
 <?php
 
+//used to output colored text
+function color_print($color, $tag, $msg) {
+  return print("<$tag style='color:$color;'>$msg</$tag><br/>");
+}
 
-// url = /form/content/newpost_req.php
+if (!empty($_POST["title"]) && !empty($_POST["content"])) {
+  //If both of them are not empty, do something here...
+  $title = $_POST["title"];
+  $content = $_POST["content"];
+} else {
+  //if title is empty print
+  if (empty($_POST["title"])) {
+    color_print("#f00", "h1", "error, title is empty");
+  }
 
-$_POST["title"];
-$_POST["content"];
+  //if content is empty print
+  if (empty($_POST["content"])) {
+    color_print("#f00", "h1", "error, content is empty");
+  }
+}
+?>
+
+</body>
+
+</html>
