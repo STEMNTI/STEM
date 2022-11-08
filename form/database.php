e76f82e1aece926180887d3412e490def7928101
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-11-09 00:45:31 +0100

    Update database.php

--- form/database.php
+++ form/database.php
@@ -1,36 +1,41 @@
 <?php
 require_once 'conf/config.php';
 
-
 $dsn = "mysql:host=$servername;dbname=$dbname;port=$port";
 
-class Database {
-    function __constructor() {
-        Global $dsn;
-        Global $password;
-        Global $username;
-        Global $userstable;
+class Database
+{
+    function __constructor()
+    {
+        global $dsn;
+        global $password;
+        global $username;
+        global $userstable;
         $this->username = $username;
         $this->password = $password;
         $this->dsn = $dsn;
         $this->userstable = $userstable;
-    
 
-    function connect($dsn, $username, $password) {
-        try {
-            $conn = new PDO($dsn, $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
-        } catch (PDOException $e) {
-            $error = "Error: " . $e->getMessage();
-            return $error;
-        }}
-
-        $this->conn = connect($this->dsn, $this->username, $this->password);
+        function connect($dsn, $username, $password)
+        {
+            try {
+                $conn = new PDO($dsn, $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $conn;
+            } catch (PDOException $e) {
+                // print("Error: " . $e->getMessage());
+                return ("Error: " . $e->getMessage());
+            }
         }
 
-    public function dbconnect() {
-        return $this->conn;
+        $this->conn = connect($this->dsn, $this->username, $this->password);
+    }
 
-}}
+    public function dbconnect()
+    {
+        return $this->conn;
+    }
+}
+
+
 ?>
