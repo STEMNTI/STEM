ddd8a1515ac08bb49e7813f87d4ad9be8626f97a
DeSalsa <Eidam.holgaard@elev.ga.ntig.se>
2022-11-14 14:11:17 +0100

    forms and stuff
    login form finished, proceeding with register form right now

--- frontend/js.js
+++ frontend/js.js
@@ -1,13 +1,16 @@
 let visLog = 0;
 window.addEventListener("load", function() {
     login_box = document.getElementById('login-box');
+    register_box = document.getElementById('register-box');
 })
  function ShowAndHide() {
     if (visLog % 2 == 1) {
         login_box.style.display = 'none';
+        register_box.style.display = 'inital';
         visLog--;
     } else {
         login_box.style.display = 'initial';
+        register_box.style.display = 'none';
         visLog++;
     }
 };  
