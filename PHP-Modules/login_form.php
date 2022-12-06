65b6980cc5ca60568cd4f219e078339bdd2a0f6e
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2022-12-06 12:42:55 +0100

    Update login_form.php

--- PHP-Modules/login_form.php
+++ PHP-Modules/login_form.php
@@ -1,32 +1,30 @@
-<?php
-session_start();
-require "./Database.php";
+ <?php
+ session_start();
+ require "./Database.php";
 
-if(!isset($_SESSION["valid"])) {
-    $_SESSION["valid"] = false;
-}
+ if (!isset($_SESSION["valid"])) {
+     $_SESSION["valid"] = false;
+ }
 
-// url = /form/login_req.php
+ // url = /form/login_req.php
 
-$_POST["username"] = "username2";
-$_POST["password"] = "12345";
+ $_POST["username"] = "username2";
+ $_POST["password"] = "12345";
 
-$query = "SELECT * FROM `users`";
+ $query = "SELECT * FROM `users`";
 
+ if (isset($conn)) {
+     $result = $conn->query($query);
+ }
+ $response = $result->fetchAll(PDO::FETCH_ASSOC);
 
+ foreach ($response as $value) {
+     if ($value["username"] == $_POST["username"]) {
+         if ($value["password"] == $_POST["password"]) {
+             $_SESSION["valid"] = true;
+             echo "Login Successful";
+         }
+     }
+ }
 
-if (isset($conn)) {
-    $result = $conn->query($query);
-}
-$response = $result->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-foreach($response as $value) {
-    if($value["username"] == $_POST["username"]) {
-        if ($value["password"] == $_POST["password"]) {
-            $_SESSION["valid"] = true;
-            echo "Login Successful";
-        }
-    }
-}
+?>
