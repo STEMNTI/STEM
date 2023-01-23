5be3eb21abc671e12f2217c2e56636278118a9b1
Ragnar <ragnar.martisson@elev.ga.ntig.se>
2023-01-23 14:16:35 +0100

    Small updatte some fps changes

--- PostSystem/post-create.php
+++ PostSystem/post-create.php
@@ -3,15 +3,16 @@ session_name("create");
 session_start();
 require("post-sql.php");
 
+// TEMP
+$_SESSION["USERNAME"] = "test";
 
-// INSERT INTO `post` (`id`, `name`, `time`, `tpost`, `post`) VALUES (NULL, 'user', current_timestamp(), 'spodfgmdskfmng', 'sdgoisdgosdg');
 
 //Detta systemet skapar ett inläg.
 //Det är väldigt likt regestreringssystemet en skilnad är att här tar du namnet på användaren som är inlogad och läger in det i databasen.if(isset($_SESSION["user"])) { 
 
         $query = sql("INSERT INTO `post` (`id`, `name`, `time`, `tpost`, `post`) VALUES (NULL, :user, current_timestamp(), :tpost, :post);", [
             
-            ":user" => "Conect login here",
+            ":user" => $_SESSION["USERNAME"],
             ":post" => $_POST["post"],
             ":tpost" => $_POST["tpost"]
         ]);
