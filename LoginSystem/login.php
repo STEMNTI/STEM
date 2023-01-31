8cd8f94a19d316408ea68d2f94a374fa300a0a9f
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-01-31 11:34:04 +0100

    v0.6b

--- LoginSystem/login.php
+++ LoginSystem/login.php
@@ -3,11 +3,11 @@ require("sql.php"); // hämtar sql.php
 require("security.php");
 
 if (isset($_POST["username"])) { //kollar om username har skickats
-    // remove dark art from the post data to prevent (dark art) sql injection
+    // Convert any special characters present in the POST data 
     $username = htmlspecialchars($_POST["username"]);
     $password = htmlspecialchars($_POST["password"]);
 
-    // select the encrypted name and pass from the users table where name and pass match encrypted post values
+    // select the encrypted username and password from the users table where name and pass match encrypted post values
     $result = sql("SELECT username, password FROM users WHERE username = :name AND password = :pass", [
         ":name" => AES256_Encrypt_CBC($username),
         ":pass" => AES256_Encrypt_CBC($password),
@@ -15,13 +15,13 @@ if (isset($_POST["username"])) { //kollar om username har skickats
 
     // if a matching row was found in users table (encrypted)
     if (isset($result[0]["username"]) && isset($result[0]["password"])) {
-        // decrypt encrypted name and pass values from database
+        // decrypt the encrypted username and password that we got from the database values from database
         $rawName = AES256_Decrypt_CBC($result[0]["username"]);
         $rawPass = AES256_Decrypt_CBC($result[0]["password"]);
 
-        // compare decrypted db info with (anti dark art) post info
+        // compare decrypted db info with the (htmlspecialchars converted) POST data 
         if ($rawName === $username && $rawPass === $password) {
-            // set clientName session variable and redirect to account page
+            // set username session variable and redirect to account page
             $_SESSION["username"] = $rawName;
 
             header("Location: homepage.php");
--- readme.md
+++ readme.md
@@ -1,20 +1,23 @@
 # STEM-Project
 
-v.4
 
-Back-END
+# changelog 
 
-Done today: Register-system, Login-system 
+#### v0.6b 
+- Merged style.css with index.css
+    - Replaced all calls to "crypt" with AES256bit implementation
+      > AES256bit implementation includes secure encryption and decryption with unique random IV/key per encryption. Key and IV are encrypted with XOR and stored in the ciphertext. then read and decrypted using XOR and used to decrypt the rest of cipher text  decrypted
 
-To-do v.5: Inlägg, Adnmin privelegier, Databose
+#### v0.4
+- (Backend) Whats New? 
+  - Register-system
+  - Login-system 
 
+- To do list:
+  - Inlägg
+  - Admin Permissions
+  - Database
 
+- (Frontend)Whats New? 
+  - Vi har fixat github hos alla och vi har alla valt en upgift vi ska arbeta på och vissa har gjort klart upgifter medans andra är pågående. 
 
-
-
-Front-END
-
-Vi har fixat github hos alla och vi har alla valt en upgift vi ska arbeta på och vissa har gjort klart upgifter medans andra är pågående. 
-
-
-v.5
