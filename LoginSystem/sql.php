ac4ce5a1bc2d6938c287d3fd9fe1b3704e36028e
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-02-06 13:03:31 +0100

    Update sql.php

--- LoginSystem/sql.php
+++ LoginSystem/sql.php
@@ -1,4 +1,21 @@
 <?php
+
+function sql_make($sql, $vals=[]){
+    // Create a PDO instance
+    $pdo = new PDO("mysql:host=localhost;dbname=STEM-login;charset=utf8;", "root", "");//ansluta till databasen
+    // Prepare the SQL query
+    $q = $pdo->prepare($sql);
+    
+    // Bind the values to the query
+    foreach ($vals as $k => $v) {
+        $q->bindValue($k, $v, PDO::PARAM_STR);
+    }
+    // Execute the query
+    $q->execute();
+    
+    // Return the results
+    return $q->fetchAll(PDO::FETCH_ASSOC);
+}/*
 $pdo = new PDO("mysql:host=localhost;dbname=STEM-login;charset=utf8;", "root", "");//ansluta till databasen
 $pdo->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//ifall det är ett error
 function sql($sql, $vals = []){//utför påståendet
@@ -9,5 +26,5 @@ function sql($sql, $vals = []){//utför påståendet
     }
     $q->execute();// utföra satsen 
     return $q->fetchAll(PDO::FETCH_ASSOC);///hämta alla rader i resultatuppsättningen
-}
+}*/
 ?>
