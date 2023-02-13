141054278e762be501e15042eda118c78152cfd4
Aurorate <milton.cronvall@gmail.com>
2023-02-13 14:00:35 +0100

    post

--- PostSystem/post-create.php
+++ PostSystem/post-create.php
@@ -10,7 +10,7 @@ $_SESSION["USERNAME"] = "test";
 //Detta systemet skapar ett inläg.
 //Det är väldigt likt regestreringssystemet en skilnad är att här tar du namnet på användaren som är inlogad och läger in det i databasen.
 
-        $query = sql("INSERT INTO posts (user, currenttime, title, posttxt, img) VALUES (:user, current_timestamp(), :tpost, :post, :img);", [
+        $query = sql("INSERT INTO posts (name, time, tpost, post, Image) VALUES (:user, current_timestamp(), :tpost, :post, :img);", [
             
             ":user" => $_SESSION["USERNAME"],
             ":post" => $_POST["post"],
