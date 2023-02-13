b621bbdd34bbf3ee2ec10c507b16ac7176f6607e
Ragnar <ragnar.martisson@elev.ga.ntig.se>
2023-02-13 13:41:35 +0100

    Merge branch 'main' of https://github.com/STEMNTI/STEM

--- PostSystem/post-post.php
+++ PostSystem/post-post.php
@@ -9,7 +9,7 @@ $posts = array_reverse($posts = sql("SELECT * FROM post", [
 
 //FOR EACH POST, PRINT VALUES (CAN POST INTO DIV FOR STYLING).
 foreach($posts as $post) {
-    echo "<div>".$post["tpost"]."<br>".$post["post"]."<br>".$post["name"]."<br>".$post["time"]."<br><img src=/PostSystem/Images/' " . $post["Image"] . " ' alt=''></div>";
+    echo "<div>".$post["tpost"]."<br>".$post["post"]."<br>".$post["name"]."<br>".$post["time"]."<br><img src='/STEM/PostSystem/Images/" . $post["Image"] . "' alt=''></div>";
 }
 ?>   
 
