c8de23745fec96acc4ec38827c48d6e0d7a229c2
Aurorate <milton.cronvall@gmail.com>
2023-02-13 13:04:15 +0100

    Post filter

--- PostSystem/post-sql.php
+++ PostSystem/post-sql.php
@@ -12,17 +12,11 @@ function sql($sql, $valslista = []) {
         foreach($valslista as $k => $v) { // Lopar igenom all k keys och all v values i arayn vals
             $query->bindParam($k, $valslista[$k]);
         }
-<<<<<<< HEAD
         $query->execute(); //Kör koden
         return $query->fetchAll(PDO::FETCH_ASSOC); //Hämtar koden
     } catch (PDOException $err) {
-=======
-        $quarry->execute(); //Kör koden
-        return $quarry->fetchAll(PDO::FETCH_ASSOC); //Hämtar koden
-    } catch (Exception $err) {
->>>>>>> 02ec48b619b277bd69523d9c292f06d128145c24
         print_r($err);
     }
     
 }
-?>
+?>          
