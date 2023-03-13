5d7d709ae036633278a36f1daa8ee988086aa183
DeSalsa <Eidam.holgaard@elev.ga.ntig.se>
2023-03-13 13:21:17 +0100

    login + register koppling

--- register.php
+++ register.php
@@ -4,26 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logga in</title>
+    <title>Register</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body class="login-body">
     <div class="login">
-        <form action="./loginSystem/login.php" method="post">
-            <h1>Logga In</h1>
+        <form action="./loginSystem/register.php" method="post">
+            <h1>Register</h1>
             <div class = "email">
-                <lable>Username</lable>
+                <label>Username</label>
                 <input type="text" name="username" placeholder="Användarnamn" class="input">
             </div>
             <div class = "password">
-                <lable>Password</lable>
+                <label>Password</label>
                 <input type="password" name="password" placeholder="Ditt lösenord" class="input">
+                <label>Confirm Password</label>
+                <input type="password" name="confirmPassword" placeholder="Confirm password" class="input">
             </div>
-            <input type="submit" name="submit" value="Logga in" class="knappar">
-            <a href="register.php">Register</a>
-            <?php
-                
-            ?>
+            <input type="submit" name="submit" value="Register" class="knappar">
+            <a href="login.php">Login</a>
         </form>        
     </div>
 </body>
