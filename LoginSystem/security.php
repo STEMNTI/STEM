d54fe6c2aa9ce6a03764ec2cba78dedf391a91f6
̸̨̡͉̯̟͇̗͈͙͙̝͍̽̾̒Ă̶̺̝̞̊́́̏̈͌̾̍̀̈́̂̋̔̇͐̔̀͘̚͜ ̴̢͈̜̦͔̦̦̖̗͚̦̈́ͅZ̶̫̥̭͎̝̻̮̘̯̳̿ͅ ̴͈̅͊͒͂͌̆͂͠͠H̴̨̛̺͕̳̱͙̩̻̩̖̲͙̘̓̾̌ͅͅ ̷͎̥̖̬̼͎̹̼̭̺̪̯̱̥̖̄̊̈́̀̃̀̂̐͊̐̄̄́̅͂̈́̋̚͠͝À̸̢̝̖͇̤̹̥͙͎̳̼̬̲̰̼̩͛̀̒̋͛̈́̊ ̴̣̹̩̥̺̙̪͎͕̭̟̲͇̘̺̈́̎̿̇́͝R̷̢̢̧̡̢̲̺̳̖̹͈̙̰̙̥̼̗͍̒̌ͅ <96749372+a0zhar@users.noreply.github.com>
2023-03-13 14:48:05 +0100

    Update security.php

--- LoginSystem/security.php
+++ LoginSystem/security.php
@@ -1,22 +1,33 @@
 <?php
 
-const AES_256_KEYLEN = 32;
-const AES_256_IV_LEN = 16;
-const XOR_MASTER_KEY = "vQa5JIjBlagq2e39wtHo7gQQ1HOEjJEQ";
-const AES_256_MODE   = "AES-256-CBC";
-
-
-// can be used to both decrypt and encrypt data
-// using basic xor encryption and decryption.
-function xorCrypt($plainData) {
-    $cipherbuf = ""; //This will contain the ciphered text.
-    // Now we go through the plain data and xor each letter one by one.
-
-    for ($i = 0; $i < strlen($plainData); $i++) {
-        $cipherbuf .= 
-        $plainData[$i] ^ XOR_MASTER_KEY[$i % strlen(XOR_MASTER_KEY)];
-    }
-    return $cipherbuf; // Return the cipher text
+/*
+IV (Explaination):
+An initialization vector (IV) is added to the encryption process. 
+The IV is a sequence of random numbers and symbols. that is added to the message before it is encrypted. 
+This means that even if you encrypt the same message with the same key multiple times, 
+the resulting scrambled messages will be different each time.
+This makes it harder for someone to figure out the key and unscramble the message.
+*/
+// Updated (New Method) 
+function AESEncryptV2($plain) {
+   // This line generates a random key using the OpenSSL library function openssl_random_pseudo_bytes(). The key is 32 bytes (256 bits) long. 
+    $key    = openssl_random_pseudo_bytes(32);
+    //This line generates a random initialization vector (IV) using the OpenSSL library function openssl_random_pseudo_bytes(). 
+    //The length of the IV is determined by the AES-256-CBC cipher, which is the cipher mode being used in this implementation.
+    $iv     = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+    //This line encrypts the plaintext using the AES-256-CBC cipher with the generated key and IV. The result is stored in the $cipher variable.
+    $cipher = openssl_encrypt($plain, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
+    return $key.$iv.$cipher;
+}
+function AESDecryptV2($ciphertext) {
+    //This line extracts the first 32 bytes of the $ciphertext string, which correspond to the key used for encryption.
+    $key = substr($ciphertext, 0, 32);
+    //This line extracts the next portion of the $ciphertext string, which corresponds to the initialization vector used 
+    //for encryption. The length of the IV is determined by the AES-256-CBC cipher, which is the cipher
+    $initVector = substr($ciphertext, 32, openssl_cipher_iv_length('AES-256-CBC'));
+    //This line extracts the remaining portion of the $ciphertext string, which corresponds to the encrypted message.
+    $cipher = substr($ciphertext, 32 + openssl_cipher_iv_length('AES-256-CBC'));
+    return openssl_decrypt($cipher, 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
 }
 
 function AES256_Encrypt_CBC($plain) {
@@ -30,3 +41,4 @@ function AES256_Decrypt_CBC($ciphertext) {
     $iv  = "123456789abcdefg";
     return openssl_decrypt($ciphertext, "AES-256-CBC", $key, 0, $iv);
 }
+?>
