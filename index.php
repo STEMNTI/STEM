fbb88b26b518bfa243619957b4bce06cdf115ded
Noobstrr <Fabian.malmstrom@gmail.com>
2023-03-20 13:41:16 +0100

    fefaffafa

--- index.php
+++ index.php
@@ -13,13 +13,13 @@ require("header.php");
         <article>           
         <div class="slideshow-container">
             <div class="mySlides fade">
-                <img src="https://img.koket.se/standard-mega/kebab-i-pitabrod-med-tva-saser-inlagd-chili-och-krispig-sallad.jpg" style="width:100%">
+                <img src="https://img.koket.se/standard-mega/kebab-i-pitabrod-med-tva-saser-inlagd-chili-och-krispig-sallad.jpg" style="width:100%;">
             </div>
             <div class="mySlides fade">
-                <img src="https://receptfavoriter.se/sites/default/files/styles/recipe_4x3/public/hemgjort_kebabkott_pa_fat_1060.jpg" style="width:100%">
+                <img src="https://receptfavoriter.se/sites/default/files/styles/recipe_4x3/public/hemgjort_kebabkott_pa_fat_1060.jpg" style="width:100%;">
             </div>
             <div class="mySlides fade">
-                <img src="https://www.bellybalance.se/wp-content/uploads/2017/05/Vegetarisk-Shish-kebab-med-quinoasallad.jpg" style="width:100%">
+                <img src="https://www.bellybalance.se/wp-content/uploads/2017/05/Vegetarisk-Shish-kebab-med-quinoasallad.jpg" style="width:100%;">
             </div>
         </div>
             <br>
