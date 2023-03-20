65568157700ddb991b8ed99c0a34e70698d79247
Noobstrr <Fabian.malmstrom@gmail.com>
2023-03-20 12:55:57 +0100

    header + footer separata filer

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StemKlubben</title>
+    <link rel="icon" type="image/x-icon" href="Bilder/logo.png">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="hamburger.css">
+    <link rel="stylesheet" href="slideshow.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+<header>
+        <img src="Bilder/logo.png" alt="Snygglogga" id="logga">
+        <h1 id="rubrik">STEM-NTI</h>
+</header>
